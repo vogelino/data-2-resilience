7af6b8248ea0e6306318c0d46eeba022157adb96
Lucas Vogel <contact@vogelino.com>
2024-07-26 17:14:26 -0400

    Add h1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,7 @@
 </script>
 
 <div class="flex h-screen w-screen flex-col items-center justify-center gap-8 bg-slate-400 p-6">
+	<h1 class="text-3xl font-bold">Data2Resilience</h1>
 	<Form />
 	<button
 		class="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"

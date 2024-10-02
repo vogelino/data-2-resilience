c51a99804dd51a8cd1c435675a943754c2f0827f
Lucas Vogel <contact@vogelino.com>
2024-10-02 13:07:52 -0300

    chore(storybook): Remove visual testing

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,13 +4,12 @@ const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|ts|svelte)'],
 	staticDirs: ['../static'],
 	addons: [
-		'@storybook/addon-svelte-csf',
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@chromatic-com/storybook',
-		'@storybook/addon-interactions',
-		'@storybook/addon-postcss'
-	],
+        '@storybook/addon-svelte-csf',
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+        '@storybook/addon-postcss'
+    ],
 	framework: {
 		name: '@storybook/sveltekit',
 		options: {}

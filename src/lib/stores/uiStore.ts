28ea4ba72a05c2a5201b5137185ec26fb1a0ed21
Lucas Vogel <contact@vogelino.com>
2024-10-07 15:57:55 -0300

    feat(LeftSidebar): Save LeftSidebar state in URL

--- src/lib/stores/uiStore.ts
+++ src/lib/stores/uiStore.ts
@@ -1,6 +1,7 @@
 import { derived, writable } from 'svelte/store';
+import { queryParam, ssp } from 'sveltekit-search-params';
 
-export const isLeftSidebarOpened = writable(true);
+export const isLeftSidebarOpened = queryParam('sidebar_open', ssp.boolean(true));
 export const toggleLeftSidebar = () => isLeftSidebarOpened.update((value) => !value);
 
 export const isRightSidebarOpened = derived(

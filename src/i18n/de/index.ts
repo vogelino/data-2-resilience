cbcfb935c317eb9a3cebdde73dbaffb440a6b5c7
alsino <iamahorse@gmail.com>
2024-11-04 09:35:01 +0100

    edtited table heading

--- src/i18n/de/index.ts
+++ src/i18n/de/index.ts
@@ -475,6 +475,7 @@ const de = {
 		},
 		stations: {
 			title: 'Wie messen wir thermischen Komfort?',
+			titleTable: 'Alle Messstationen im Überblick',
 			intro: [
 				'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.'
 			],

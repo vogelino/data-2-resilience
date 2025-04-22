a0da698fa262e246dc50fae0b277f47783c4b2c9
Lucas Vogel <contact@vogelino.com>
2025-04-22 15:49:23 +0200

    WIP: Add custom colors scales from @jknitter

--- src/lib/utils/colorScaleUtil.ts
+++ src/lib/utils/colorScaleUtil.ts
@@ -17,9 +17,17 @@ const schemeTurboSquential: readonly string[] = quantize(interpolateTurbo, 10);
 const schemeTurboOrdinal: readonly string[] = quantize(interpolateTurbo, 10);
 export const schemeBrBG: readonly string[] = quantize(interpolateRainbow, 10).toReversed();
 
-type SequentialScapeType = {
+type SequentialScapeInputType = {
 	type: 'sequential';
 	scheme: readonly string[];
+	perc5: number;
+	perc95: number;
+	validMin: number;
+	validMax: number;
+	fraction: number;
+};
+
+type SequentialScapeType = Pick<SequentialScapeInputType, 'type' | 'scheme'> & {
 	min: number;
 	max: number;
 };
@@ -33,74 +41,110 @@ export const unitsToScalesMap = {
 	default: {
 		type: 'sequential',
 		scheme: schemeBrBG,
-		min: 0,
-		max: 100
+		perc5: 0,
+		perc95: 100,
+		validMin: 0,
+		validMax: 100,
+		fraction: 1
 	},
 	air_temperature: {
 		type: 'sequential',
 		scheme: quantize(interpolateSpectral, 10).toReversed(),
-		min: -20,
-		max: 50
+		perc5: 0,
+		perc95: 24,
+		validMin: -Infinity,
+		validMax: Infinity,
+		fraction: 0.25
 	},
 	absolute_humidity: {
 		type: 'sequential',
 		scheme: quantize(interpolateBlues, 10),
-		min: 0,
-		max: 30
+		perc5: 3,
+		perc95: 14,
+		validMin: 0,
+		validMax: Infinity,
+		fraction: 0.3
 	},
 	atmospheric_pressure: {
 		type: 'sequential',
 		scheme: quantize(interpolateYlGnBu, 10),
-		min: 950,
-		max: 1050
+		perc5: 983,
+		perc95: 1016,
+		validMin: 860,
+		validMax: 1055,
+		fraction: 0.3
 	},
 	atmospheric_pressure_reduced: {
 		type: 'sequential',
 		scheme: quantize(interpolateYlGnBu, 10),
-		min: 950,
-		max: 1050
+		perc5: 998,
+		perc95: 1031,
+		validMin: 860,
+		validMax: 1055,
+		fraction: 0.3
 	},
 	dew_point: {
 		type: 'sequential',
 		scheme: quantize(interpolateBlues, 10),
-		min: -10,
-		max: 30
+		perc5: -3,
+		perc95: 17,
+		validMin: -Infinity,
+		validMax: Infinity,
+		fraction: 0.3
 	},
 	heat_index: {
 		type: 'sequential',
 		scheme: quantize(interpolateReds, 10),
-		min: 20,
-		max: 110
+		perc5: -3,
+		perc95: 25,
+		validMin: -Infinity,
+		validMax: Infinity,
+		fraction: 0.3
 	},
 	lightning_average_distance: {
 		type: 'sequential',
 		scheme: quantize(interpolateYlOrBr, 10),
-		min: 0,
-		max: 20
+		perc5: 0,
+		perc95: 15,
+		validMin: 0,
+		validMax: Infinity,
+		fraction: 0.3
 	},
 	lightning_strike_count: {
 		type: 'sequential',
 		scheme: quantize(interpolateReds, 10),
-		min: 0,
-		max: 100
+		perc5: 0,
+		perc95: 30,
+		validMin: 0,
+		validMax: Infinity,
+		fraction: 0.3
 	},
 	maximum_wind_speed: {
 		type: 'sequential',
 		scheme: quantize(interpolateViridis, 10),
-		min: 0,
-		max: 40
+		perc5: 0,
+		perc95: 8,
+		validMin: 0,
+		validMax: Infinity,
+		fraction: 0.3
 	},
 	mrt: {
 		type: 'sequential',
 		scheme: quantize(interpolateSpectral, 10).toReversed(),
-		min: -10,
-		max: 50
+		perc5: -4,
+		perc95: 44,
+		validMin: -Infinity,
+		validMax: Infinity,
+		fraction: 0.3
 	},
 	pet: {
 		type: 'sequential',
 		scheme: schemeTurboSquential,
-		min: -15,
-		max: 28
+		perc5: -5,
+		perc95: 25,
+		validMin: -Infinity,
+		validMax: Infinity,
+		fraction: 0.7
 	},
 	pet_category: {
 		type: 'ordinal',
@@ -109,26 +153,38 @@ export const unitsToScalesMap = {
 	precipitation_sum: {
 		type: 'sequential',
 		scheme: quantize(interpolateBlues, 10),
-		min: 0,
-		max: 200
+		perc5: 0,
+		perc95: 5,
+		validMin: 0,
+		validMax: Infinity,
+		fraction: 0.1
 	},
 	relative_humidity: {
 		type: 'sequential',
 		scheme: quantize(interpolateBlues, 10),
-		min: 0,
-		max: 100
+		perc5: 3,
+		perc95: 14,
+		validMin: 0,
+		validMax: Infinity,
+		fraction: 0.3
 	},
 	solar_radiation: {
 		type: 'sequential',
 		scheme: quantize(interpolateYlOrBr, 10),
-		min: 0,
-		max: 1000
+		perc5: 0,
+		perc95: 603,
+		validMin: 0,
+		validMax: 1400,
+		fraction: 0.3
 	},
 	utci: {
 		type: 'sequential',
 		scheme: schemeTurboSquential,
-		min: -40,
-		max: 45
+		perc5: -3,
+		perc95: 24,
+		validMin: -Infinity,
+		validMax: Infinity,
+		fraction: 0.7
 	},
 	utci_category: {
 		type: 'ordinal',
@@ -137,28 +193,82 @@ export const unitsToScalesMap = {
 	vapor_pressure: {
 		type: 'sequential',
 		scheme: quantize(interpolateYlGnBu, 10),
-		min: 0,
-		max: 40
+		perc5: 4,
+		perc95: 16,
+		validMin: 0,
+		validMax: Infinity,
+		fraction: 0.3
 	},
 	wet_bulb_temperature: {
 		type: 'sequential',
 		scheme: quantize(interpolateSpectral, 10),
-		min: 0,
-		max: 50
+		perc5: -2,
+		perc95: 16,
+		validMin: -Infinity,
+		validMax: Infinity,
+		fraction: 0.3
 	},
 	wind_direction: {
 		type: 'sequential',
 		scheme: schemeBrBG,
-		min: 0,
-		max: 360
+		perc5: 0,
+		perc95: 4,
+		validMin: 0,
+		validMax: Infinity,
+		fraction: 0.3
 	},
 	wind_speed: {
 		type: 'sequential',
 		scheme: quantize(interpolateBlues, 10),
-		min: 0,
-		max: 40
+		perc5: 0,
+		perc95: 4,
+		validMin: 0,
+		validMax: Infinity,
+		fraction: 0.3
 	}
-} satisfies Record<string, SequentialScapeType | OrdinalScaleType>;
+} satisfies Record<string, SequentialScapeInputType | OrdinalScaleType>;
+
+interface ColorMapRange {
+	type: 'range';
+	scheme: string[];
+	min: number;
+	max: number;
+}
+
+function computeColormapRanges(
+	values: Array<number | null>,
+	paramObj: SequentialScapeInputType
+): [number, number] {
+	const filteredValues = values.filter((val) => val !== null) as number[];
+	const dataMin = Math.min(...filteredValues);
+	const dataMax = Math.max(...filteredValues);
+	const dataRange = dataMax - dataMin;
+	const expectedRange = paramObj.perc95 - paramObj.perc5;
+	const minimumRange = Math.abs(expectedRange * paramObj.fraction);
+
+	let vMin = dataMin;
+	let vMax = dataMax;
+
+	if (dataRange < minimumRange) {
+		if (dataRange === 0) {
+			vMin = dataMin - minimumRange / 2;
+			vMax = dataMax + minimumRange / 2;
+		} else {
+			const valDiff = minimumRange - dataRange;
+			vMin = dataMin - valDiff / 2;
+			vMax = dataMax + valDiff / 2;
+		}
+	}
+
+	if (vMin < paramObj.perc5 && isFinite(paramObj.perc5)) {
+		vMin = paramObj.perc5;
+	}
+	if (vMax > paramObj.perc95 && isFinite(paramObj.perc95)) {
+		vMax = paramObj.perc95;
+	}
+
+	return [vMin, vMax];
+}
 
 type ColorStop = { color: string; position: number };
 type ColorStops = ColorStop[];
@@ -211,7 +321,11 @@ export const getColorStops = (params: { unit: string; LL: TranslationFunctions }
 	return stops;
 };
 
-export function getColorScaleFn(params: { unit: string; LL: TranslationFunctions }) {
+export function getColorScaleFn(params: {
+	unit: string;
+	LL: TranslationFunctions;
+	values: (number | string)[];
+}) {
 	const { unit } = params;
 	const unitConfig = getUnit(unit);
 	if (unitConfig.type === 'ordinal') return scaleOrdinal(unitConfig.scheme);
@@ -224,9 +338,11 @@ export function getColorScaleFn(params: { unit: string; LL: TranslationFunctions
 			.interpolate(interpolateRgb);
 		return scale(t);
 	}
-	const domain = isHealthRiskUnit(unit)
-		? getHealthRiskUnitBounds(unit as HealthRiskUnit)
-		: [unitConfig.min, unitConfig.max];
+	const allNumValues = params.values.filter((v) => typeof v === 'number');
+	const domain =
+		isHealthRiskUnit(unit) && params.values.every((v) => typeof v === 'number')
+			? getHealthRiskUnitBounds(unit as HealthRiskUnit)
+			: computeColormapRanges(allNumValues, unitConfig);
 	return scaleSequential(customInterpolator).domain(domain);
 }
 
@@ -234,6 +350,7 @@ export function getColorScaleValue(params: {
 	unit: string;
 	LL: TranslationFunctions;
 	value: number | string;
+	values: (number | string)[];
 }) {
 	const { unit, LL, value } = params;
 

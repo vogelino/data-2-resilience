69de43f7f3c01b8f0e68f3da1514be569d366f87
Jonas Kittner <jonas.kittner@rub.de>
2025-04-27 19:23:47 +0200

    update all texts
    Co-authored-by: Stefan Schmidt <Stefan.Schmidt-y9u@ruhr-uni-bochum.de>

--- src/lib/components/SensorDescription.svelte
+++ src/lib/components/SensorDescription.svelte
@@ -10,13 +10,37 @@
 			src: string;
 			alt: string;
 		};
-		supportedIndicators: string[];
+		calculatedIndicators: string[];
+		measuredIndicators: string[];
+		descriptionCalculated: string;
 	}
 
-	const { title, description, image, supportedIndicators }: Props = $props();
+	const {
+		title,
+		description,
+		image,
+		calculatedIndicators,
+		measuredIndicators,
+		descriptionCalculated
+	}: Props = $props();
 
 	const indicators = $derived(
-		supportedIndicators
+		calculatedIndicators
+			.reduce(
+				(acc, i) => {
+					const units = $LL.pages.measurements.unitSelect.units;
+					const value = i as keyof typeof units;
+					const label = units[value]?.label();
+					const unitOnly = units[value]?.unitOnly();
+					if (label) return [...acc, { value, label, unitOnly }];
+					return acc;
+				},
+				[] as { value: string; label: string; unitOnly: string }[]
+			)
+			.sort((a, b) => sortUnitByLabel(a, b, $locale))
+	);
+	const measuredIndicatorsList = $derived(
+		measuredIndicators
 			.reduce(
 				(acc, i) => {
 					const units = $LL.pages.measurements.unitSelect.units;
@@ -40,7 +64,21 @@
 		<CollapsibleParagraph linesClampedCount={3} className="text-sm">
 			<p>{description}</p>
 			<strong class="mb-1 mt-4 block text-base font-semibold">
-				{$LL.pages.stations.stationsDescriptions.supportedIndicatorsLabel()}:
+				{$LL.pages.stations.stationsDescriptions.measuredIndicatorsLabel()}:
+			</strong>
+			<ul class="flex flex-col gap-1 pb-4">
+				{#each measuredIndicatorsList as { value, label, unitOnly } (value)}
+					<li class="border-b border-muted pb-1">
+						{label}
+						{#if unitOnly}
+							<span class="text-muted-foreground"> ({unitOnly})</span>
+						{/if}
+					</li>
+				{/each}
+			</ul>
+			<p>{descriptionCalculated}</p>
+			<strong class="mb-1 mt-4 block text-base font-semibold">
+				{$LL.pages.stations.stationsDescriptions.calculatedIndicatorsLabel()}:
 			</strong>
 			<ul class="flex flex-col gap-1 pb-4">
 				{#each indicators as { value, label, unitOnly } (value)}

48d2231d46dffd8c1b1f2dfa3ec44f6e7de62801
Jonas Kittner <jonas.kittner@rub.de>
2025-05-28 11:46:48 +0200

    add sentry error monitoring

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
+import { sentrySvelteKit } from '@sentry/sveltekit';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sentrySvelteKit({
+			sourceMapsUploadOptions: {
+				org: 'rubclim',
+				project: 'd2r-dashboard'
+			}
+		}),
+		sveltekit()
+	]
 });
